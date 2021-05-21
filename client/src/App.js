d2a734cc36047c337ea16448173a7755b485f965
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-21 20:20:43 +0530

    second comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,32 @@
-import { useState, useEffect, } from 'react';
+import React, { useState, useEffect, } from 'react';
 import axios from 'axios';
-
+import LanginPage from './pages/LandingPage/LandingPage';
 
 function App() {
-    const [data, setData] = useState([]);
-    const [product, setProduct] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [componentTrigger, setComponentTrigger] = useState(false);
+  const [data, setData] = useState([]);
+  const [product, setProduct] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [componentTrigger, setComponentTrigger] = useState(false);
 
-    const productName = (event) => {
-        setProduct(event.target.value);
-    }
-    const pwdName = (event) => {
-        setPwd(event.target.value);
-    }
-    useEffect(() => {
-        axios.get('/user').then((res) => {
-            console.log(res);
-            setData(res.data.productList);
-            setComponentTrigger(false);
-        });
-    }, [componentTrigger]);
+  const productName = (event) => {
+    setProduct(event.target.value);
+  }
+  const pwdName = (event) => {
+    setPwd(event.target.value);
+  }
+  useEffect(() => {
+    axios.get('/user').then((res) => {
+      console.log(res);
+      setData(res.data.productList);
+      setComponentTrigger(false);
+    });
+  }, [componentTrigger]);
 
-    return (
-        <div className="hellocomponenet">
-            <div className="listofproducts">
-              <p> Data! </p>
-                {data.map((item, index) => {
-                    return (
-                        <div className="lp">
-                            <p>
-                                <br /> <b>Product Name:</b> {item.name + " "} <br /> <b>Price: </b>{item.price}
-                                <br />
-                                {/* <button id={item._id} onClick={deleteData} > Delete  </button>  &nbsp;
-                                <button onClick={updateMe} id={item._id} > Update click here!  </button>
-                                <div className="updateForm">
-                                    <b>Name:</b><input type="text" value={product} onChange={productName} id="name" placeholder="name" /> <br />
-                                    <b>Price:</b><input type="text" value={pwd} onChange={pwdName} id="price" placeholder="price" /> <br />
-                                    <button id={item._id} onClick={updateForm} Form > Update details  </button>
-                                </div> */}
-                            </p>
-                        </div>
-                    );
-                })}
-            </div>
-
-
-        </div>
-    );
+  return (
+    <React.Fragment>
+      <LanginPage />
+    </React.Fragment>
+  );
 }
 
 export default App;
