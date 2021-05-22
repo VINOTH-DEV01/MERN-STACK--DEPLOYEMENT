0d78f346259630bbcac531e36a8497d3a7fa5cc3
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-22 21:04:38 +0530

    login and register screen add!

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,47 +2,61 @@ import React from 'react';
 import { useState, useEffect, } from 'react';
 import axios from 'axios';
 import './Login.css';
+import { Link } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
+    },
+}));
 
 function Login() {
+    const classes = useStyles();
+
     return (
         <React.Fragment>
             <div className="login-pg">
-                <div className="container">
-                    <div className="demo-title">
-                        <h3> Login </h3>
-                        <p> Welcome to Vidu! Please enter your credential to login </p>
-                        <div className="vidu-brd">
-                            <span></span><span></span><span></span>
-                        </div>
-                    </div>
-                    <div className="login-form textbox-h38">
-                        <div className="row inputtextanimation ">
-                            <div id="loginform">
-                                <div className="col-sm-12">
-                                    <div className="form-group">
-                                        <label for="exampleInputEmail1"> EMAIL ID</label>
-                                        <input type="email" className="form-control" id="useremailid" aria-describedby="emailHelp" placeholder="Enter your WORK email ID here*" required="" />
-                                    </div>
-                                </div>
-                                <div className="col-sm-12">
-                                    <div className="form-group">
-                                        <label for="exampleInputEmail1">Password</label>
-                                        <input type="password" className="form-control" id="loginpwd" aria-describedby="emailHelp" placeholder="XXXXXXXXXXXX " required="" />
-                                    </div>
-                                </div>
-                                <div className="col-sm-12 text-center">
-                                    <div className="btn btn-default xlbtn" id="loginbtn-block">
-                                        LOGIN
-						                  </div>
-                                </div>
-                                <div className="col-sm-12 forgotpwd">
-                                    <span> New User? <u> <a href="./register.php"> Register Here! </a> </u> </span>
-                                    <span> <u id="forgetPasswordSubmit" > Forgot Password? </u></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <TextField
+                    id="outlined-full-width"
+                    label="First Name"
+                    style={{ margin: 8 }}
+                    placeholder="Type Here"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-full-width"
+                    label="Last Name"
+                    style={{ margin: 8 }}
+                    placeholder="Type Here"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    type="password"
+                    variant="outlined"
+                />
+
+                <Button variant="contained" color="primary">
+                    Login
+                </Button>  &nbsp;
+                <Link to={'/register'} >
+                    <Button variant="contained" color="primary">
+                        Register
+                </Button>
+                </Link>
             </div>
         </React.Fragment>
     );
